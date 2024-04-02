cfcc6b8ec9b985cd00bd70cebe0c0c60a6b6bda0
Lorena González Cordero <lore_383@hotmail.com>
2024-04-02 11:26:17 +0200

    feat(angularv17): Day 1

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -22,6 +22,7 @@ export class HomeComponent implements OnInit {
 
   public characterName = '';
   public characters: any;
+  characters$ = this.apiService.getAllCharacters();
 
   constructor(private apiService: ApiService) {}
 
