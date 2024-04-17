b931999c4ce7b4cda100649055a4f363cc1fcac8
Lorena González Cordero <lore_383@hotmail.com>
2024-04-17 11:16:07 +0200

    feat(lazy-loading): Configuring routes to lazy load components.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -7,15 +7,40 @@ import { ProductDetailsComponent } from './features/products/details/details.com
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
 export const routes: Routes = [
-    { path: 'home', title: 'Home', component: HomeComponent },
-    { path: 'home/detail/:id', component: DetailComponent },
-    { path: 'home/cart', component: CartComponent },
-    { path: 'home/products', component: ProductsComponent },
-    { path: 'home/product-details/:id', component: ProductDetailsComponent },
-    { path: 'home/products', component: ProductsComponent, children: [
-        { path: 'details/:id', component: ProductDetailsComponent }
-    ]},
-    { path: 'detail/:id', component: DetailComponent },
-    { path: '', redirectTo: 'home', pathMatch: 'full' },
-    { path: '**', component: PageNotFoundComponent },
+    {
+        path: 'home',
+        title: 'Home',
+        loadComponent: () => import('./components/home/home.component').then(c => c.HomeComponent)
+    }, {
+        path: 'home/detail/:id',
+        loadComponent: () => import('./components/detail/detail.component').then(c => c.DetailComponent)
+    }, {
+        path: 'home/cart',
+        loadComponent: () => import('./features/cart/cart.component').then(c => c.CartComponent)
+    }, {
+        path: 'home/products',
+        loadComponent: () => import('./features/products/products.component').then(c => c.ProductsComponent)
+    }, {
+        path: 'home/product-details/:id',
+        loadComponent: () => import('./features/products/details/details.component').then(c => c.ProductDetailsComponent)
+    }, {
+        path: 'home/products',
+        loadComponent: () => import('./features/products/products.component').then(c => c.ProductsComponent),
+        children: [
+            {
+                path: 'details/:id',
+                loadComponent: () => import('./features/products/details/details.component').then(c => c.ProductDetailsComponent)
+            }
+        ]
+    }, {
+        path: 'detail/:id',
+        loadComponent: () => import('./components/detail/detail.component').then(c => c.DetailComponent)
+    }, {
+        path: '',
+        redirectTo: 'home',
+        pathMatch: 'full'
+    }, {
+        path: '**',
+        loadComponent: () => import('./page-not-found/page-not-found.component').then(c => c.PageNotFoundComponent)
+    }
 ];
